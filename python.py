8e2222d93ba30cf59809218db73fcf9795a520df
commonhonesty <commonhonesty@hanmail.net>
2020-01-30 23:28:34 +0900

    My Django Girls app, first commit

---
+++ python.py
@@ -0,0 +1,99 @@
+2 + 3
+
+2 ** 3
+
+"Ola"
+
+"Hi there" + "Ola"
+
+"Ola" * 3
+
+"Runnin' down the hill"
+
+'Runnin\' down the hill'
+
+"Ola".upper()
+
+len("Ola")
+
+len(str(204023))
+
+name = "Ola"
+name
+
+name = "Sonja"
+name
+
+len(name)
+
+a = 4
+b = 6
+a * b
+
+city = "Tokyo"
+city
+
+name = 'Maria'
+name
+print(name)
+
+[]
+
+lottery = [3, 42, 12, 19, 30, 59]
+len(lottery)
+
+lottery.sort()
+print(lottery)
+
+lottery.reverse()
+print(lottery)
+
+lottery.append(199)
+print(lottery)
+
+print(lottery[0])
+print(lottery[1])
+
+print(lottery)
+print(lottery[0])
+lottery.pop(0)
+print(lottery)
+
+{}
+participant = {'name': 'Ola', 'country': 'Poland', 'favorite_numbers': [7, 42, 92]}
+print(participant['name'])
+
+participant['age']
+
+participant['favorite_language'] = 'Python'
+len(participant)
+
+participant.pop('favorite_numbers')
+participant
+
+participant['country'] = 'Germany'
+participant
+
+5 > 2
+3 < 1
+5 > 2 * 2
+1 == 1
+5 != 2
+
+6 >= 12 / 2
+3 <= 2
+
+6 > 2 and 2 < 3
+3 > 2 and 2 < 1
+3 > 2 or 2 < 1
+
+a = True
+a
+
+a = 2 > 5
+a
+
+True and True
+False and True
+True or 1 == 1
+1 != 2
